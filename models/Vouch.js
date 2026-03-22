8fe6e0715425502c9f1028b5a763f3108a168994
Ariftxn <121342224+Ariftxn@users.noreply.github.com>
2026-03-22 20:42:56 +0700

    Create models/Vouch.js

---
+++ models/Vouch.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const VouchSchema = new mongoose.Schema({
+    voucherId: { type: String, required: true },
+    voucheeId: { type: String, required: true },
+    reason: { type: String, required: true },
+    color: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+// Index untuk mempercepat pencarian (Anti-Dupe)
+VouchSchema.index({ voucherId: 1, voucheeId: 1 });
+
+module.exports = mongoose.model('Vouch', VouchSchema);
